d50b14278fefaa6626803283295a9a2b4c38fc8c
manuel (Manuel J. A. Eugster) <manuel.eugster@stat.uni-muenchen.de>
2011-10-19 09:30:03 +0000

    concentration measures.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/soccer@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/concentration.R
+++ pkg/R/concentration.R
@@ -1,5 +1,32 @@
 
-
+#' Concentration indices
+#'
+#' This is a proof-of-concept implementation of concentration measures
+#' for data with an exogenous order.
+#'
+#' @param x Data vector
+#'
+#' @return The concenctration index.
+#'
+#' @examples
+#'   ## See demo("concentration-bundesliga") for a real-world
+#'   ## application.
+#'
+#'   set.seed(1234)
+#'   x <- sample(100000, 10)
+#'
+#'   ## Classical concentration indices:
+#'   herfindahl(x)
+#'   rosenbluth(x)
+#'
+#'   ## Exogenous order is available:
+#'   o <- sample(10)
+#'   exogeny(x, o)
+#'
+#' @family concentration
+#' @rdname concentration_indices
+#' @aliases concentration_indices
+#'
 #' @export
 herfindahl <- function(x) {
   y <- sumex(x, ex = order(x))^2
@@ -9,6 +36,7 @@ herfindahl <- function(x) {
 }
 
 
+#' @rdname concentration_indices
 #' @export
 rosenbluth <- function(x) {
   n <- length(x)
@@ -21,8 +49,10 @@ rosenbluth <- function(x) {
 }
 
 
+#' @param ex Order of the data vector
+#' @rdname concentration_indices
 #' @export
-exogeny <- function(x, ex) {
+exogeny <- function(x, ex = order(x)) {
   n <- length(x)
 
   y <- sumex(x, ex = ex)
@@ -33,8 +63,28 @@ exogeny <- function(x, ex) {
 }
 
 
+#' Concentration ratios
+#'
+#' This is a proof-of-concept implementation of concentration ratios
+#' for data with an exogenous order.
+#'
+#' @param x Data vector
+#' @param g Number of "first" data (according to the order)
+#' @param ex Exogenous order of the data vector
+#' @return The concentration ratio value
+#'
+#' @examples
+#'   set.seed(1234)
+#'   x <- sample(100000, 10)  # Data
+#'   o <- sample(10)          # Exogenous order
+#'
+#'   concentration_ratio(x, 3)
+#'   concentration_ratio(x, 3, ex = o)
+#'
+#' @family concentration
+#'
 #' @export
-concentration_ratio <- function(x, g, ex = order(x)) {
+concentration_ratio <- function(x, g, ex = order(x, decreasing = TRUE)) {
   y <- sumex(x, ex = ex)
   y <- sum(y[1:g])
 
@@ -42,7 +92,6 @@ concentration_ratio <- function(x, g, ex = order(x)) {
 }
 
 
-#' @export
 sumex <- function(x, ex) {
   x <- x[ex]
   x / sum(x)
